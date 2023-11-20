f45a2ce5bc63e7fae01fae4158801ee02fa52200
Ydemae <113693952+Ydemae@users.noreply.github.com>
2023-11-20 09:10:06 +0100

    Abalyse
    Analyse du jeu snake donné en exemple sur raylib pour essayer de mieux comprendre le fonctionnement

--- main.cpp
+++ main.cpp
@@ -9,14 +9,19 @@ int main()
 {
     const int screenWidth = 800;
     const int screenHeight = 600;
+    Font fonts[5] = {0};
+    fonts[0] = LoadFont("resources/fonts/alagard.png");
+
 
     InitWindow(screenWidth, screenHeight, "Test");
     SetTargetFPS(60);
+    while (!WindowShouldClose()){
+        BeginDrawing();
+        ClearBackground(RAYWHITE);
+        DrawTextEx(fonts[0] , "Yay", (Vector2){100, 100} , fonts[0].baseSize, 0, BLACK);
+        EndDrawing();
+    }
 
-    BeginDrawing();
-    ClearBackground(RAYWHITE);
-    DrawText("Yay", 100, 100, 50, BLACK);
-    EndDrawing();
 
     return 0;
 }
--- run.ps1
+++ run.ps1
@@ -1,4 +1,5 @@
-g++ main.cpp -o main.exe -Ol -Wall -std=c99 -Wno-missing-braces -I include/ -L Lib/ -lraylib -lopengl32 -lgdi32 -lwinmm
+g++ main.cpp -o main.exe -Wall -std=c++20 -Wno-missing-braces -I include/ -L Lib/ -lraylib -lopengl32 -lgdi32 -lwinmm
 
 
 gcc main.c -o main.exe -Wall -std=c99 -Wno-missing-braces -I include/ -L Lib/ -lraylib -lopengl32 -lgdi32 -lwinmm
+
