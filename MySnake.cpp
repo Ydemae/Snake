86ebc7250407868e5666539cadeda25382e0c7dc
Ydemae <113693952+Ydemae@users.noreply.github.com>
2023-11-23 10:55:08 +0100

    movements and grid
    Amélioration de la grid et ajout des mouvements du serpent au sein de la grid

--- MySnake.cpp
+++ MySnake.cpp
@@ -78,9 +78,15 @@ int Ediv(int a, int b);
 
 void initGame(int screenHeight, int screenWidth);
 
+void updateGame(int screenHeight, int screenWidth);
+
+int drawGame(int screenHeight, int screenWidth);
+
 static Snake snake[MAXSNAKELENGTH] = {};
 static Fruit fruit;
-static int count = 0;
+static bool gameOver = false;
+static bool gamePaused = false;
+static int InputBuffer = -1;
 
 
 int main(){
@@ -101,26 +107,116 @@ int Ediv(int a, int b){
 }
 
 void initGame(int screenHeight, int screenWidth){
-    snake[0] = Snake((Vector2){0+SQUARESIZE,0+SQUARESIZE}, (Vector2){SQUARESIZE, SQUARESIZE}, (Vector2){0,0}, (Color)RED);
+    snake[0] = Snake((Vector2){0+SQUARESIZE*2,0+SQUARESIZE*2}, (Vector2){SQUARESIZE, SQUARESIZE}, (Vector2){0,0}, (Color)RED);
+    snake[0].setSpeed((Vector2){Ediv(SQUARESIZE,10), 0});
 }
 
 int update(int screenHeight, int screenWidth){
-    int GridHeight = Ediv(screenHeight, SQUARESIZE);
-    int GridWidth = Ediv(screenWidth, SQUARESIZE);
+    updateGame(screenHeight, screenWidth);
+    drawGame(screenHeight, screenWidth);
+    return 0;
+}
+
+void updateGame(int screenHeight, int screenWidth){
+    if (!gameOver){
+        int gridHeight = Ediv(screenHeight, SQUARESIZE);
+        int gridWidth = Ediv(screenWidth, SQUARESIZE);
+        bool canMove = false;
+        if (IsKeyPressed(KEY_D) || IsKeyPressed(KEY_RIGHT) || InputBuffer == 0){
+            if (snake[0].getSpeed().x == 0){
+                for (int i = 0; i < gridHeight; i++){
+                    if (snake[0].getPosition().y == i*SQUARESIZE){
+                        canMove = true;
+                    }
+                }
+                if (canMove){
+                    snake[0].setSpeed((Vector2){Ediv(SQUARESIZE,10), 0});
+                    InputBuffer = -1;
+                    canMove = false;
+                }
+                else{
+                    InputBuffer = 0;
+                }
+            }
+        }
+        if (IsKeyPressed(KEY_A) ||IsKeyPressed(KEY_LEFT) || InputBuffer == 1){
+            if (snake[0].getSpeed().x == 0){
+                for (int i = 0; i < gridHeight; i++){
+                    if (snake[0].getPosition().y == i*SQUARESIZE){
+                        canMove = true;
+                    }
+                }
+                if (canMove){
+                    snake[0].setSpeed((Vector2){-Ediv(SQUARESIZE,10), 0});
+                    InputBuffer = -1;
+                    canMove = false;
+                }
+                else{
+                    InputBuffer = 1;
+                }
+            }
+        }
+        if (IsKeyPressed(KEY_W) || IsKeyPressed(KEY_UP) || InputBuffer == 2){
+           if (snake[0].getSpeed().y == 0){
+                for (int i = 0; i < gridWidth; i++){
+                    if (snake[0].getPosition().x == i*SQUARESIZE){
+                        canMove = true;
+                    }
+                }
+                if (canMove){
+                    snake[0].setSpeed((Vector2){0, -Ediv(SQUARESIZE,10)});
+                    InputBuffer = -1;
+                    canMove = false;
+                }
+                else{
+                    InputBuffer = 2;
+                }
+            }
+        }
+        if (IsKeyPressed(KEY_S) ||IsKeyPressed(KEY_DOWN) || InputBuffer == 3){
+             if (snake[0].getSpeed().y == 0){
+                for (int i = 0; i < gridWidth; i++){
+                    if (snake[0].getPosition().x == i*SQUARESIZE){
+                        canMove = true;
+                    }
+                }
+                if (canMove){
+                    snake[0].setSpeed((Vector2){0, Ediv(SQUARESIZE,10)});
+                    InputBuffer = -1;
+                    canMove = false;
+                }
+                else{
+                    InputBuffer = 3;
+                }
+            }
+        }
+        for(int i = 0; i < ACTUALSIZE; i++){
+            Vector2 actualpos = snake[i].getPosition();
+            Vector2 actualspeed =  snake[i].getSpeed();
+            Vector2 NewPos = (Vector2){actualpos.x + actualspeed.x, actualpos.y + actualspeed.y};
+            snake[i].setPosition(NewPos);
+        }
+    }
+}
+
+int drawGame(int screenHeight, int screenWidth){
+    int GridHeight = Ediv(screenHeight, SQUARESIZE) - 1;
+    int GridWidth = Ediv(screenWidth, SQUARESIZE) - 1;
+    Vector2 offset;
+    offset.x = SQUARESIZE * 2;
+    offset.y = SQUARESIZE * 2;
     BeginDrawing();
     ClearBackground(ColorFromNormalized((Vector4){0.258, 0, 0}));
-    int petitCompteurdeLignes = 0;
-    for (int i = 0; i < GridHeight; i++){
-        DrawLine(0, i*SQUARESIZE , screenWidth, i*SQUARESIZE, WHITE);
-        petitCompteurdeLignes++;
+    for (int i = 2; i < GridHeight; i++){
+        DrawLine(0 + offset.y, i*SQUARESIZE , screenWidth - offset.x, i*SQUARESIZE, WHITE);
     }
-    for (int i = 0; i < GridWidth; i++){
-            DrawLine(i*SQUARESIZE, 0, i*SQUARESIZE, screenHeight , WHITE);
-            petitCompteurdeLignes++;
-        }
+    for (int i = 2; i < GridWidth; i++){
+            DrawLine(i*SQUARESIZE, 0 + offset.x, i*SQUARESIZE, screenHeight - offset.y , WHITE);
+    }
     for (int i = 0; i < ACTUALSIZE; i++){
         DrawRectangleV(snake[i].getPosition(), snake[i].getSize(), snake[i].getColor());
     }
+    //DrawText(TextFormat("Yay %d %d", offset.x, offset.y ), 100 - MeasureText("Yay", 30), 100 - MeasureText("Yay", 30), 30, WHITE);
     EndDrawing();
     return 0;
 }
--- run.ps1
+++ run.ps1
@@ -1,5 +1,6 @@
 g++ main.cpp -o main.exe -Wall -std=c++20 -Wno-missing-braces -I include/ -L Lib/ -lraylib -lopengl32 -lgdi32 -lwinmm
 
+g++ MySnake.cpp -o MySnake.exe -Wall -std=c++20 -Wno-missing-braces -I include/ -L Lib/ -lraylib -lopengl32 -lgdi32 -lwinmm
 
 gcc main.c -o main.exe -Wall -std=c99 -Wno-missing-braces -I include/ -L Lib/ -lraylib -lopengl32 -lgdi32 -lwinmm
 
