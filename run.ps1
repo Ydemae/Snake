e086c2525f8f4cb83a314683f3f0976b312c314b
Ydemae <113693952+Ydemae@users.noreply.github.com>
2023-12-12 17:17:18 +0100

    Delete run.ps1

--- run.ps1
+++
@@ -1,9 +0,0 @@
-g++ main.cpp -o main.exe -Wall -std=c++20 -Wno-missing-braces -I include/ -L Lib/ -lraylib -lopengl32 -lgdi32 -lwinmm
-
-g++ MySnake.cpp -o MySnake.exe -Wall -std=c++20 -Wno-missing-braces -I include/ -L Lib/ -lraylib -lopengl32 -lgdi32 -lwinmm
-
-gcc main.c -o main.exe -Wall -std=c99 -Wno-missing-braces -I include/ -L Lib/ -lraylib -lopengl32 -lgdi32 -lwinmm
-
-
-Pour sans console
-g++ MySnake.cpp -o MySnake.exe -Wall -mwindows -std=c++20 -Wno-missing-braces -I include/ -L Lib/ -lraylib -lopengl32 -lgdi32 -lwinmm
